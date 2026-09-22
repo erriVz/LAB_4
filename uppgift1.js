93653701f6e41858b8ef7d0b6cf628963b6de710
Victor <victor@heras.nu>
2026-09-22 17:48:08 +0200

    Lagt till att ålder och skrivs ut i console.log i uppgift1

--- uppgift1.js
+++ uppgift1.js
@@ -3,5 +3,5 @@ enamn = "Heras"
 ålder = 25
 student = true
 
-console.log("Mitt namn är", namn,enamn,);
+console.log("Mitt namn är", namn,enamn,"och är",ålder,"år gammal");
 console.log("Student:",student);
