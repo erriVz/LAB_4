4221c99f2bfd8a67868ce3e81804b6f541bfe906
Victor <victor@heras.nu>
2026-09-22 17:55:10 +0200

    Skapade två variabler (antal&pris) i uppgift2 som sedan skrivs ut separat samt total pris för samtliga samt totalt+moms

---
+++ uppgift2.js
@@ -0,0 +1,6 @@
+antal = 10
+pris = 5
+console.log("Pris:",pris,"kr");
+console.log("Antal:",antal);
+console.log("Totala priset för alla produkter är", antal*pris,"kr");
+console.log("Totalt + moms(25%):", antal*pris*1.25,"kr");
