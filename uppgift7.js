a4823728a0aa39bcf4456eb23fbbf28bc3084f18
Victor <victor@heras.nu>
2026-09-23 21:16:15 +0200

    ändrade argument i funktionen

--- uppgift7.js
+++ uppgift7.js
@@ -1,9 +1,9 @@
 tal = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 
-function summan_array(tal){
+function summan_array(arr){
     summan = 0;
-    for(let i = 0; i < tal.length; i++) {
-        summan += tal[i]
+    for(let i = 0; i < arr.length; i++) {
+        summan += arr[i]
     }
     return summan;
 } 
