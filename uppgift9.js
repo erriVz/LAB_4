5c3297a9f161fe4fd4e1d0b6986d0ada42704c82
Victor <victor@heras.nu>
2026-09-23 20:43:50 +0200

    skapade objekt i uppgift9

---
+++ uppgift9.js
@@ -0,0 +1,17 @@
+Hushåll = [
+    {
+        namn: "Victor",
+        ålder: 25,
+        familjetag: "Son"
+    },
+    {
+        namn: "Ricardo",
+        ålder: 57,
+        familjetag: "Far"
+    },
+    {
+        namn: "Zeb",
+        ålder: 1,
+        familjetag: "Hund"
+    }
+];
