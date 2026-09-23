6c66c4dd46f1711d13e6a0707d6737f874d7a6a4
Victor <victor@heras.nu>
2026-09-23 20:01:00 +0200

    Fixade namn på funktionen samt namn på argumenten i funktioneni uppgift6

--- uppgift6.js
+++ uppgift6.js
@@ -1,7 +1,7 @@
-function area(a, b){
-    return a*b;
+function rektangel_area(bredd, höjd){
+    return bredd*höjd;
 }
 
-console.log("Arean är " + area(3, 5));
-console.log("Arean är " + area(10, 23));
-console.log("Arean är " + area(9, 2));
+console.log("Arean är " + rektangel_area(3, 5));
+console.log("Arean är " + rektangel_area(10, 23));
+console.log("Arean är " + rektangel_area(9, 2));
