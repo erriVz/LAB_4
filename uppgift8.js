282f278704a3df039d0d5006af015d96e4847769
Victor <victor@heras.nu>
2026-09-23 20:38:59 +0200

    Skapade objekt i uppgift8 samt funktion som skriver informationen i objektet

---
+++ uppgift8.js
@@ -0,0 +1,13 @@
+bok = {
+    titel: "The Last Wish",
+    författare: "Andrzej Sapkowski",
+    utgivningsår: 1993
+}
+
+function om_bok(bok){
+    console.log("Titel: " + bok.titel);
+    console.log("Författare: " + bok.författare);
+    console.log("Utgivingsår: " + bok.utgivningsår);    
+}
+
+om_bok(bok)
