ebaea40e2c74a947639750235021190ceb901080
Victor <victor@heras.nu>
2026-09-24 11:13:54 +0200

    Kommentar ändrad i uppgift7

--- uppgift7.js
+++ uppgift7.js
@@ -5,7 +5,7 @@
 
 let tal = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 
-function summan_array(arr){ //loopar genom arrayen och lägger till varje värde till variabeln "summa"
+function summan_array(arr){ //Funktion som loopar genom arrayen och lägger till varje värde till variabeln "summa"
     let summan = 0;
     for(let i = 0; i < arr.length; i++) {
         summan += arr[i]
